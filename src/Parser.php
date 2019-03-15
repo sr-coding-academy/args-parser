88e426a55d6806367c9a8a75409acbe2141ffd49
LukasBru <brunner.lukas@live.com>
2019-03-15 14:55:21 +0100

    checks for first flag and value

--- src/Parser.php
+++ src/Parser.php
@@ -16,6 +16,20 @@ class Parser
 
     public function parse($value)
     {
-
+        if ($pos = strpos($value, '-')) {
+            $flag = substr($value, $pos + 1, 2);
+            if (in_array($flag, $this->flags)) {
+                if (strpos($value, '-', $pos + 3)) {
+                    $subvalue = substr($value, $pos + 3, strpos($value, '-') - $pos + 3);
+                } else {
+                    $subvalue = substr($value, $pos + 3);
+                }
+                $results[] = new Substring($flag, $subvalue);
+            } else {
+                echo 'No valid Flag found!';
+            }
+        } else {
+            echo 'No valid Flag found!';
+        }
     }
 }
