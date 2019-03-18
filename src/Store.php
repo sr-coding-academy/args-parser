69f7018d5cd6e3604006b59bc6335841e42710b9
r.quidet <r.quidet@sportradar.com>
2019-03-18 14:26:52 +0100

    saves flags and values.

--- src/Store.php
+++ src/Store.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: r.quidet
- * Date: 18.03.2019
- * Time: 08:39
- */
 
 namespace ArgsParser;
 
-
 class Store
 {
+    private $register = [];
 
+    public function __construct()
+    {
+
+    }
+
+    public function addValuesToRegister($array)
+    {
+        foreach ($array as $item) {
+            $this->register[] = array(
+                substr($item, 0, 1) => array(
+                    ltrim(substr($item, strpos($item, ' '), strlen($item)))
+                    )
+            );
+        }
+    }
+
+    public function addAllowedFlags($array)
+    {
+        foreach ($array as $item) {
+            $this->allowedFlags[] = substr($item, 0, 1);
+        }
+    }
+
+    public function addValue()
+    {
+
+    }
+
+    public function getRegister()
+    {
+        return $this->register;
+    }
 }
