756644be0c247edb889140d618bdb47dd69788a4
r.quidet <r.quidet@sportradar.com>
2019-03-18 14:26:36 +0100

    parses commands.

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ require("vendor/autoload.php");
 
 use ArgsParser\Parser;
 
+$allowedFlags = "u,d,p";
+$validator = new Validator($allowedFlags);
 
-$input = "-u root";
-$parser = new Parser($input);
+$input = "-u ralph -u michi -u root";
+$parser = new Parser($input, $validator);
+$parser->ask("u");
+
+//RETURN root
+//$parser->ask(u);
--- src/Parser.php
+++ src/Parser.php
@@ -2,8 +2,55 @@
 
 namespace ArgsParser;
 
-
 class Parser
 {
+    private $validator;
+    private $store;
+
+    public function __construct($input, Validator $validator)
+    {
+        $this->validator = $validator;
+        $this->store = new Store();
+        $this->parse($input);
+    }
+
+    public function parse($input)
+    {
+        if ($this->validator->validate($input)) {
+            $splitInput = explode("-", $input);
+            $trimmedInput = $this->trimInput($splitInput);
+            $this->store->addValuesToRegister($trimmedInput);
+            var_dump($this->store->getRegister());
+            if ($this->validator->validateArray($trimmedInput)) {
+                //TODO: RESOLVE
+            }
+            //TODO: Entscheiden, ob 2x checken oder 1x
+        }
+    }
+
+    private function trimInput($array)
+    {
+        $trimmed = [];
+        foreach ($array as $item) {
+            $trimmed[] = rtrim($item);
+        }
+        return $trimmed;
+    }
+
+    public function ask($flag)
+    {
+        if (in_array("u", $this->validator->getAllowedFlags())) {
+            $tmp = $this->store->getRegister();
+            echo $tmp['u'];
+        } elseif (in_array("d", $this->validator->getAllowedFlags())) {
+            $tmp = $this->store->getRegister();
+            echo $tmp['d'];
+        } elseif (in_array("p", $this->validator->getAllowedFlags())) {
+            $tmp = $this->store->getRegister();
+            echo $tmp['p'];
+        } else {
+            echo "invalid flag";
+        }
+    }
 
 }
