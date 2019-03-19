f4fa82b7ae9d1cfb82dc53e8d3bb3d941983a845
LukasBru <brunner.lukas@live.com>
2019-03-19 11:59:39 +0100

    displays output by schema

--- index.php
+++ index.php
@@ -4,5 +4,4 @@ use ArgsParser\Parser;
 use ArgsParser\Display;
 
 $parser = new Parser("-u root -l -d /usr/lo-gs -p 8080 -f file.txt,script.sh,data.json -i 1,-2,-3,-4");
-$display = new Display();
-$display->displayResult($parser->flags);
+$display = new Display($parser->flags, '-u -i -d');
--- src/Display.php
+++ src/Display.php
@@ -8,16 +8,27 @@ class Display
 {
     private $schema;
 
-    public function __construct($schema = '-u -d -p -l -f -i')
+    public function __construct($flags, $schema = '-u -d -p -l -f -i')
     {
         $this->schema=$schema;
+        $this->generateOutput($flags);
     }
 
-    public function displayResult($flags)
+    public function displayResult($flag)
     {
-        foreach ($flags as $flag) {
-            echo $flag->getDescription() . ": ";
-            var_dump($flag->getValue());
+        echo $flag->getDescription() . ": ";
+        var_dump($flag->getValue());
+    }
+
+    private function generateOutput($flags)
+    {
+        $schema=explode(' ', $this->schema);
+        foreach($schema as $flagName){
+            foreach($flags as $flag){
+                if($flagName === $flag->getName()){
+                    $this->displayResult($flag);
+                }
+            }
         }
     }
 }
